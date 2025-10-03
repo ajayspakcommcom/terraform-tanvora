b77dab4cf9fb189b1d23118eb40b9730c03fdff5
Ajay Spak <ajayspakcommcom>
2025-10-03 13:21:35 +0530

    changes

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1" # Change to your AWS region
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "my-first-terraform-bucket-ajay"
+  acl    = "private"
+
+  tags = {
+    Name        = "MyFirstBucket"
+    Environment = "Dev"
+  }
+}
